4c6a50a3a3d864941bffb8b8718e3e8d7a3bbab5
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2020-06-05 17:07:46 +0200

    Update GridExtensions.cs

--- Assets/Scripts/C2M2/NeuronalDynamics/Visualization/Mapping/GridExtensions.cs
+++ Assets/Scripts/C2M2/NeuronalDynamics/Visualization/Mapping/GridExtensions.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace C2M2.NeuronalDynamics.UGX
 {
@@ -14,7 +15,41 @@ namespace C2M2.NeuronalDynamics.UGX
         /// </summary>
         /// <param name="name">Name of subset</param>
         public static int[] GetSubsetIndices(this Grid grid, in string name) => grid.Subsets[name].Indices;
+        
+        /// <summary>
+        /// Returns the name of the subset the vertex belongs to
+        /// <param name="grid">Name of grid</param>
+        /// <param name="vertex">Vertex</param>
+        /// </summary>
+        /// Returns false if subset not present in any available subset in this grid
+        public static bool GetSubsetName(this Grid grid, in Vertex vertex, out string subsetName) {
+          foreach(KeyValuePair<string, Subset> subset in grid.Subsets.subsets) {
+            int[] indices = GetSubsetIndices(grid, subset.Key);
+            if (indices.Contains(vertex.Id)) {
+              subsetName = subset.Key;
+            }
+          }
+          subsetName = "UNASSIGNED ELEMENT";
+          return false;
+        }
 
+        /// <summary>
+        /// Returns the name of the subset the vertex index belongs to
+        /// <param name="grid">Name of grid</param>
+        /// <param name="id">Vertex id</param>
+        /// </summary>
+        /// Returns false if subset not present in any available subset in this grid
+        public static bool GetSubsetName(this Grid grid, in int index, out string subsetName) {
+          foreach(KeyValuePair<string, Subset> subset in grid.Subsets.subsets) {
+            int[] indices = GetSubsetIndices(grid, subset.Key);
+            if (indices.Contains(index)) {
+              subsetName = subset.Key;
+              return true;
+            }
+          }
+          subsetName = "UNASSIGNED ELEMENT";
+          return false;
+        }
 
         /// <summary>
         /// Returns the name of a subset
