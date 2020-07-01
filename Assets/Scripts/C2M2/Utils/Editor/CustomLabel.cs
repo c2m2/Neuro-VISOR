885a50f6ea518ae3e2530940d3185913828d4e13
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2020-07-01 08:06:46 +0200

    Update CustomLabel.cs
    Commenting CustomLabel

--- Assets/Scripts/C2M2/Utils/Editor/CustomLabel.cs
+++ Assets/Scripts/C2M2/Utils/Editor/CustomLabel.cs
@@ -10,29 +10,35 @@ namespace C2M2.Utils.Editor
     /// </summary>
     public class CustomLabel : PropertyAttribute
     {
+        /// label name
         public readonly string label;
 
-        public CustomLabel(string label)
-        {
-            this.label = label;
-        }
+        /// <summary>
+        /// Constructs a custom label
+        /// </summary>
+        /// <param name="label"> Label text </param>
+        public CustomLabel(string label) => this.label = label;
     }
 }
+
+
 namespace C2M2.Utils.Editor
 {
 #if UNITY_EDITOR
     /// <summary>
-    ///
+    /// Custom property drawer for CustomLabel classes
+    /// This will allow to add custom label text for properties in 
+    /// the UnityEditor'S inspector. Also arrays of properties are supported.
     /// </summary>
     [CustomPropertyDrawer(typeof(CustomLabel))]
     internal class ThisPropertyDrawer : PropertyDrawer
     {
         /// <summary>
-        ///
+        /// Set the label text of a specified property during creation of GUI 
         /// </summary>
-        /// <param name="position"></param>
-        /// <param name="property"></param>
-        /// <param name="label"></param>
+        /// <param name="position"> position on GUI </param>
+        /// <param name="property"> GUI property </param>
+        /// <param name="label"> Name of label to display</param>
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             try
@@ -59,17 +65,16 @@ namespace C2M2.Utils.Editor
         }
 
         /// <summary>
-        ///
+        /// Helper method to check if or not a serializable property is of array type or not
         /// </summary>
-        /// <param name="property"></param>
-        /// <returns></returns>
+        /// <param name="property">A property </param>
+        /// <returns> bool </returns>
         private bool IsArray(SerializedProperty property)
         {
             string path = property.propertyPath;
             int idot = path.IndexOf('.');
             if (idot == -1) return false;
             return property.serializedObject.FindProperty(path.Substring(0, idot)).isArray;
-
         }
     }
 #endif
