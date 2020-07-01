6616bea31bb048622941e97a65d4d98fcce7d1eb
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2020-07-01 08:27:26 +0200

    Update Graph.cs
    Commenting Graph.

--- Assets/Scripts/C2M2/NeuronalDynamics/Visualization/Mapping/Graph.cs
+++ Assets/Scripts/C2M2/NeuronalDynamics/Visualization/Mapping/Graph.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 namespace C2M2.NeuronalDynamics.Alg
 {
     /// <summary>
-    /// Very simple class graph
+    /// Very simple class representing a graph consisting of vertices and edges of a given type T
     /// </summary>
+    /// <typeparam="T"> vertex type </typeparam>
     public class Graph<T>
     {
         public Graph() { }
+        /// <summary>
+        /// Construct a graph out of a list of vertices and edges
+        /// </summary>
         public Graph(IEnumerable<T> vertices, IEnumerable<Tuple<T, T>> edges)
         {
             foreach (var vertex in vertices)
@@ -18,13 +22,22 @@ namespace C2M2.NeuronalDynamics.Alg
                 AddEdge(edge);
         }
 
+        /// Store adjacency list
         public Dictionary<T, HashSet<T>> AdjacencyList { get; } = new Dictionary<T, HashSet<T>>();
 
+        /// <summary>
+        /// Add a vertex to the graph
+        /// </summary>
+        /// <param name="vertex"> A vertex </param>
         public void AddVertex(T vertex)
         {
             AdjacencyList[vertex] = new HashSet<T>();
         }
-
+        
+        /// <summary>
+        /// Add an edge to the graph
+        /// </summary>
+        /// <param name="edge"> edge </param>
         public void AddEdge(Tuple<T, T> edge)
         {
             if (AdjacencyList.ContainsKey(edge.Item1) && AdjacencyList.ContainsKey(edge.Item2))
@@ -40,6 +53,11 @@ namespace C2M2.NeuronalDynamics.Alg
     /// </summary>
     public sealed class Algorithms
     {
+        /// <summary>
+        /// DFS search on the graph with start vertex T
+        /// </summary>
+        /// <param name="graph"> A graph </param>
+        /// <param naem="start"> Start vertex </param>
         public HashSet<T> DFS<T>(Graph<T> graph, T start)
         {
             var visited = new HashSet<T>();
