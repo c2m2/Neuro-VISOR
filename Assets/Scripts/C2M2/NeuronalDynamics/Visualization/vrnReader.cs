f34ed181a5862679571bd83ea1e070c7e741e440
Stephan Grein <grein@temple.edu>
2020-08-12 14:01:33 +0200

    Update vrnReader.cs
    Code formatting.

--- Assets/Scripts/C2M2/NeuronalDynamics/Visualization/vrnReader.cs
+++ Assets/Scripts/C2M2/NeuronalDynamics/Visualization/vrnReader.cs
@@ -8,45 +8,36 @@ using UnityEngine;
 using UnityEditor;
 #endregion
 
-namespace C2M2.NeuronalDynamics.Visualization
-{
-    namespace vrn
-    {
-        public sealed class vrnReader : MonoBehaviour
-        {
-        
-                 /// GEOM1D
-                /// <summary>
-                /// Stores the 1D geometries by name and refinement 
-                /// </summary>
-                            [Serializable]
-                public struct Geom1d
-                {
-                    public string name;
-                    public string refinement;
-                }
+namespace C2M2.NeuronalDynamics.Visualization {
+    namespace vrn {
+        public sealed class vrnReader : MonoBehaviour {
 
-                /// GEOM2D
-                /// <summary>
-                /// Stores the 2d geometries by name and inflation
-                /// </summary>
-                            [Serializable]
-                public struct Geom2d
-                {
-		  public string inflation;
-                   public string name;
-                 }
-                 
+            /// GEOM1D
+            /// <summary>
+            /// Stores the 1D geometries by name and refinement
+            /// </summary>
+            [Serializable]
+            public struct Geom1d {
+                public string name;
+                public string refinement;
+            }
+
+            /// GEOM2D
+            /// <summary>
+            /// Stores the 2d geometries by name and inflation
+            /// </summary>
+            [Serializable]
+            public struct Geom2d {
+                public string inflation;
+                public string name;
+            }
+
             /// GEOMETRY
             /// <summary>
             /// Stores 1D and 2D geometries
             /// </summary>
             [Serializable]
-            public class Geometry
-            {
-	
-                
-
+            public class Geometry {
                 /// All 1D geometries
                 public Geom1d[] geom1d;
                 /// All 2D geometries
@@ -63,15 +54,13 @@ namespace C2M2.NeuronalDynamics.Visualization
             /// <summary>
             /// Loads the meta data stored in the JSON
             /// </summary>
-            private void load()
-            {
-                using (ZipArchive archive = ZipFile.OpenRead(this.fileName))
-                {
+            private void load() {
+                using (ZipArchive archive = ZipFile.OpenRead(this.fileName)) {
                     var file = archive.GetEntry("MetaInfo.json");
                     _ = file ?? throw new ArgumentNullException(nameof(file));
                     geometry = JsonUtility.FromJson<Geometry>(new StreamReader(file.Open()).ReadToEnd().ToString());
                     loaded = true;
-              
+
                 }
             }
 
@@ -79,11 +68,9 @@ namespace C2M2.NeuronalDynamics.Visualization
             /// <summary>
             /// Read a file from the .vrn archive
             /// </summary>
-            private Stream read(in string meshName)
-            {
+            private Stream read(in string meshName) {
                 string name = Path.GetTempPath() + Path.GetRandomFileName();
-                using (ZipArchive archive = ZipFile.Open(this.fileName, ZipArchiveMode.Read))
-                {
+                using (ZipArchive archive = ZipFile.Open(this.fileName, ZipArchiveMode.Read)) {
                     var file = archive.GetEntry(meshName);
                     _ = file ?? throw new ArgumentNullException(nameof(file));
                     return file.Open();
@@ -96,13 +83,12 @@ namespace C2M2.NeuronalDynamics.Visualization
             /// </summary>
             /// <param name="refinement"> Refinement number (Default: 0)</param>
             /// <returns> Filename of refined 1D mesh in archive </returns>
-            public string retrieve_1d_mesh(int refinement = 0)
-            {
+            public string retrieve_1d_mesh(int refinement = 0) {
                 if (!loaded) load();
                 int index = geometry.geom1d.ToList().FindIndex
-                (geom => Int16.Parse(geom.refinement) == refinement);
+                            (geom => Int16.Parse(geom.refinement) == refinement);
                 return geometry.geom1d[index != -1 ? index : 0].name;
-            
+
             }
 
             /// RETRIEVE_2D_MESH
@@ -111,66 +97,57 @@ namespace C2M2.NeuronalDynamics.Visualization
             /// </summary>
             /// <param name="inflation"> Inflation factor </param>
             /// <returns> Filename of inflated 2D mesh in archive </returns>
-            public string retrieve_2d_mesh(double inflation = 1.0)
-            {
+            public string retrieve_2d_mesh(double inflation = 1.0) {
                 if (!loaded) load();
-		int index = geometry.geom2d.ToList().FindIndex
-                (geom => Double.Parse(geom.inflation) == inflation);
+                int index = geometry.geom2d.ToList().FindIndex
+                            (geom => Double.Parse(geom.inflation) == inflation);
                 return geometry.geom2d[index != -1 ? index : 0].name;
             }
 
             /// VRNREADER
             /// <summary>
-            /// constructor 
+            /// constructor
             /// </summary>
             /// <param name="fileName"> Archive's file name (.vrn file) </param>
             public vrnReader(in string fileName) => this.fileName = fileName;
-        
-        
-        public void Start() {
-	  
-	  UnityEngine.Debug.Log(Application.dataPath);
-	             string fileName = Application.dataPath + "/test.vrn";
-                try
-                {
+
+
+            public void Start() {
+
+                UnityEngine.Debug.Log(Application.dataPath);
+                string fileName = Application.dataPath + "/test.vrn";
+                try {
                     vrnReader reader = new vrnReader(fileName);
                     /// Get 1d mesh (0-th refinement aka coarse grid)
                     Console.WriteLine(reader.retrieve_1d_mesh(0));
                     UnityEngine.Debug.Log(reader.retrieve_1d_mesh(0));
-                         
+
                     /// Get inflated 2d mesh by a factor of 2.5
                     ///Console.WriteLine(reader.retrieve_2d_mesh(2.5));
-                }
-                catch (Exception ex) when (ex is System.IO.FileNotFoundException || ex is System.ArgumentNullException)
-                {
-		    UnityEngine.Debug.Log("file not found!");
-		    UnityEngine.Debug.Log(ex);
+                } catch (Exception ex) when (ex is System.IO.FileNotFoundException || ex is System.ArgumentNullException) {
+                    UnityEngine.Debug.Log("file not found!");
+                    UnityEngine.Debug.Log(ex);
                     Console.WriteLine($"File not found: {fileName}!");
                     Console.WriteLine(ex);
                 }
-	    }
+            }
         }
 
         /// EXAMPLE
-        /// <summary>  
+        /// <summary>
         /// Example demonstrating vrnReader usage
         /// </summary>
-        class Example
-        {
-            static void Main(string[] args)
-            {
+        class Example {
+            static void Main(string[] args) {
                 string fileName = "test.vrn";
-                try
-                {
+                try {
                     vrnReader reader = new vrnReader(fileName);
                     /// Get 1d mesh (0-th refinement aka coarse grid)
                     Console.WriteLine(reader.retrieve_1d_mesh(0));
-                         
+
                     /// Get inflated 2d mesh by a factor of 2.5
                     Console.WriteLine(reader.retrieve_2d_mesh(2.5));
-                }
-                catch (Exception ex) when (ex is System.IO.FileNotFoundException || ex is System.ArgumentNullException)
-                {
+                } catch (Exception ex) when (ex is System.IO.FileNotFoundException || ex is System.ArgumentNullException) {
                     Console.WriteLine($"File not found: {fileName}!");
                     Console.WriteLine(ex);
                 }
