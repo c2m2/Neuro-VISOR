5de7d74c53e2ad0da48435b0f170cd9aa79b1514
jakesmells <tuf91497@temple.edu>
2020-10-27 11:44:25 -0400

    Dual grab infrastructure and action prototypes in place. Need to test in lab

---
+++ Assets/DualGrabPositioner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace C2M2.Interaction
+{
+    public class DualGrabPositioner : DualGrabAction
+    {
+        protected override IEnumerator GrabAction(Transform handA, Transform handB)
+        {
+            Vector3 relPos = transform.position - Vector3.Lerp(handA.position, handB.position, 0.5f);
+
+            while (true)
+            {
+                transform.position = relPos + Vector3.Lerp(handA.position, handB.position, 0.5f);
+                yield return null;
+            }
+        }
+    }
+}
