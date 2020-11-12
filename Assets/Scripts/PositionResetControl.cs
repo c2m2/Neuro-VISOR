e447cdb333f38155d2b3d15fbbac02268fff778b
jakesmells <tuf91497@temple.edu>
2020-11-12 16:10:19 -0500

    .

--- Assets/Scripts/PositionResetControl.cs
+++ Assets/Scripts/PositionResetControl.cs
@@ -7,8 +7,19 @@ namespace C2M2.Interaction
     public class PositionResetControl : MonoBehaviour
     {
         public OVRInput.Button resetButton = OVRInput.Button.Start;
+        public KeyCode resetKey = KeyCode.X;
         public Vector3 resetPosition = Vector3.zero;
         public Transform target = null;
+
+        private bool ResetRequested
+        {
+            get
+            {
+                return GameManager.instance.vrIsActive ?
+                    OVRInput.Get(resetButton) :
+                    Input.GetKey(resetKey);
+            }
+        }
 
         private void Start()
         {
@@ -18,7 +29,7 @@ namespace C2M2.Interaction
         // Update is called once per frame
         void Update()
         {
-            if (OVRInput.Get(resetButton))
+            if (ResetRequested)
             {
                 Debug.Log("Resetting " + target.name + "'s position to " + resetPosition.ToString("F4"));
                 target.position = resetPosition;
