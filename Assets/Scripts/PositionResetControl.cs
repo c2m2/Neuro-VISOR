9780da5d8720eda9c154d9cdae08b220ce380e61
jakesmells <tuf91497@temple.edu>
2020-11-12 15:42:04 -0500

    Addeded target reset position button on OVRInput.Button.Start button

---
+++ Assets/Scripts/PositionResetControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace C2M2.Interaction
+{
+    public class PositionResetControl : MonoBehaviour
+    {
+        public OVRInput.Button resetButton = OVRInput.Button.Start;
+        public Vector3 resetPosition = Vector3.zero;
+        public Transform target = null;
+
+        private void Start()
+        {
+            if (target == null) target = transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (OVRInput.Get(resetButton))
+            {
+                target.position = resetPosition;
+            }
+        }
+    }
+}
