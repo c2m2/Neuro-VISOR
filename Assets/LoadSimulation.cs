707b676455a4979f8c748e5700a4ac8a28dcf24c
jakesmells <tuf91497@temple.edu>
2020-12-09 15:30:26 -0500

    Simple geometry loading through UI. Need to improve greatly.

---
+++ Assets/LoadSimulation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using C2M2.NeuronalDynamics.Simulation;
+namespace C2M2.NeuronalDynamics.Interaction
+{
+    public class LoadSimulation : MonoBehaviour
+    {
+        public string vrnFileName = "10-dkvm2_1d";
+        public Gradient gradient;
+        private bool loaded = false;
+        public void Load(RaycastHit hit)
+        {
+            if (!loaded)
+            {
+                GameObject solveObj = new GameObject();
+                solveObj.name = "Solver";
+                solveObj.AddComponent<MeshFilter>();
+                solveObj.AddComponent<MeshRenderer>();
+                NDSimulation solver = solveObj.AddComponent<SparseSolverTestv1>();
+                solver.vrnFileName = vrnFileName;
+                solver.gradient = gradient;
+                solver.Initialize();
+
+                loaded = true;
+                transform.parent.gameObject.SetActive(false);
+            }
+        }
+    }
+}
