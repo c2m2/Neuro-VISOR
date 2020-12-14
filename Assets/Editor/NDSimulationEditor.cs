08a6a9d50ff408aa17bfdbe49a1cc8ae6105977d
jakesmells <tuf91497@temple.edu>
2020-12-14 17:07:44 -0500

    .

---
+++ Assets/Editor/NDSimulationEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+
+namespace C2M2.NeuronalDynamics.Simulation
+{
+    [CustomEditor(typeof(NDSimulation), true)]
+    public class NDSimulationEditor : Editor
+    {
+        static int refinementLevel;
+        static double inflationLevel;
+
+        NDSimulation sim;
+
+        public void Awake()
+        {
+            sim = target as NDSimulation;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            refinementLevel = EditorGUILayout.IntField("Refinement Level: ", sim.RefinementLevel);
+            inflationLevel = EditorGUILayout.DoubleField("Inflation Level: ", sim.VisualInflation);
+
+            sim.RefinementLevel = refinementLevel;
+            sim.VisualInflation = inflationLevel;
+            DrawDefaultInspector();
+        }
+    }
+}
--- Assets/Scripts/C2M2/NeuronalDynamics/Simulation/NDSimulation.cs
+++ Assets/Scripts/C2M2/NeuronalDynamics/Simulation/NDSimulation.cs
@@ -452,28 +452,4 @@ namespace C2M2.NeuronalDynamics.Simulation {
             return "v1: " + v1 + "\nv2: " + v2 + "\nlambda: " + lambda;
         }
     }
-
-    [CustomEditor(typeof(NDSimulation), true)]
-    public class NDSimulationEditor : Editor
-    {
-        static int refinementLevel;
-        static double inflationLevel;
-
-        NDSimulation sim;
-
-        public void Awake()
-        {
-            sim = target as NDSimulation;
-        }
-
-        public override void OnInspectorGUI()
-        {
-            refinementLevel = EditorGUILayout.IntField("Refinement Level: ", sim.RefinementLevel);
-            inflationLevel = EditorGUILayout.DoubleField("Inflation Level: ", sim.VisualInflation);
-
-            sim.RefinementLevel = refinementLevel;
-            sim.VisualInflation = inflationLevel;
-            DrawDefaultInspector();
-        }
-    }
 }
