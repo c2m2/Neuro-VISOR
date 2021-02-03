bacabf8d983e32e3bb53ab7e7e948ca82946d158
James Rosado <jarosado0911@gmail.com>
2021-02-03 15:36:35 -0500

    Modified Solver

--- Assets/Scripts/C2M2/NeuronalDynamics/Simulation/CellSolvers/SparseSolverTestv1.cs
+++ Assets/Scripts/C2M2/NeuronalDynamics/Simulation/CellSolvers/SparseSolverTestv1.cs
@@ -37,7 +37,6 @@ namespace C2M2.NeuronalDynamics.Simulation
         public double h = 0.27;                           // User enters spatial step size
         public double k = 0.0027;                         // User enters time step size
         public bool HK_auto = true;                       // auto choose H and K
-        public bool SomaOn = false;                        // set soma to be clamped to vstart
 
         //Set cell biological paramaters
         [Header("Biological Parameters")]
@@ -126,119 +125,114 @@ namespace C2M2.NeuronalDynamics.Simulation
         protected override void Solve()
         {       
             
-                int nT;                                                             // Number of Time steps
-                List<bool> channels = new List<bool> { false, false, false };       // For adding/removing channels
+            int nT;                                                             // Number of Time steps
+            List<bool> channels = new List<bool> { false, false, false };       // For adding/removing channels
 
-                // TODO: NEED TO DO THIS BETTER
-                if (HK_auto)
-                {
-                    h = 0.1 * NeuronCell.edgeLengths.Average();
-                    if (h <= 1) { k = h / 130; }                // 0 refine       
-                    if (h <= 0.5) { k = h / 65; }               // 1 refine
-                    if (h <= 0.25) { k = h / 32.5; }            // 2 refine
-                    if (h <= 0.12) { k = h / 18; }              // 3 refine
-                    if (h <= 0.06) { k = h / 9; }               // 4 refine
-                    if (h <= 0.03) { k = h / 5; }
-                }
+            // TODO: NEED TO DO THIS BETTER
+            if (HK_auto)
+            {
+                h = 0.1 * NeuronCell.edgeLengths.Average();
+                if (h <= 1) { k = h / 130; }                // 0 refine       
+                if (h <= 0.5) { k = h / 65; }               // 1 refine
+                if (h <= 0.25) { k = h / 32.5; }            // 2 refine
+                if (h <= 0.12) { k = h / 18; }              // 3 refine
+                if (h <= 0.06) { k = h / 9; }               // 4 refine
+                if (h <= 0.03) { k = h / 5; }
+            }
 
                
-                // Number of time steps
-                nT = (int)System.Math.Floor(endTime / k);
+            // Number of time steps
+            nT = (int)System.Math.Floor(endTime / k);
 
-                // set some constants for the HINES matrix
-                double diffConst = (1 / (2 * res * cap));
-                double cfl = diffConst * k / h;
+            // set some constants for the HINES matrix
+            double diffConst = (1 / (2 * res * cap));
+            double cfl = diffConst * k / h;
 
-                // reaction vector
-                Vector R = Vector.Build.Dense(NeuronCell.vertCount);
-                List<double> reactConst = new List<double> { gk, gna, gl, ek, ena, el };
+            // reaction vector
+            Vector R = Vector.Build.Dense(NeuronCell.vertCount);
+            List<double> reactConst = new List<double> { gk, gna, gl, ek, ena, el };
 
-                // temporary voltage vector
-                Vector tempV = Vector.Build.Dense(NeuronCell.vertCount);
+            // temporary voltage vector
+            Vector tempV = Vector.Build.Dense(NeuronCell.vertCount);
 
-                // Construct sparse RHS and LHS in coordinate storage format, no zeros are stored
-                List<CoordinateStorage<double>> sparse_stencils = makeSparseStencils(NeuronCell, h, k, diffConst);
+            // Construct sparse RHS and LHS in coordinate storage format, no zeros are stored
+            List<CoordinateStorage<double>> sparse_stencils = makeSparseStencils(NeuronCell, h, k, diffConst);
 
-                // Compress the sparse matrices
-                CompressedColumnStorage<double> r_csc = CompressedColumnStorage<double>.OfIndexed(sparse_stencils[0]); //null;
-                CompressedColumnStorage<double> l_csc = CompressedColumnStorage<double>.OfIndexed(sparse_stencils[1]); //null;
+            // Compress the sparse matrices
+            CompressedColumnStorage<double> r_csc = CompressedColumnStorage<double>.OfIndexed(sparse_stencils[0]); //null;
+            CompressedColumnStorage<double> l_csc = CompressedColumnStorage<double>.OfIndexed(sparse_stencils[1]); //null;
 
-                // Permutation matrix----------------------------------------------------------------------//
-                int[] p = new int[NeuronCell.vertCount];
+            // Permutation matrix----------------------------------------------------------------------//
+            int[] p = new int[NeuronCell.vertCount];
                 
-                 p = Permutation.Create(NeuronCell.vertCount, 0); 
+                p = Permutation.Create(NeuronCell.vertCount, 0); 
 
-                CompressedColumnStorage<double> Id_csc = CompressedColumnStorage<double>.CreateDiagonal(NeuronCell.vertCount, 1);
-                Id_csc.PermuteRows(p);
-                //--------------------------------------------------------------------------------------------//
+            CompressedColumnStorage<double> Id_csc = CompressedColumnStorage<double>.CreateDiagonal(NeuronCell.vertCount, 1);
+            Id_csc.PermuteRows(p);
+            //--------------------------------------------------------------------------------------------//
 
-                // for solving Ax = b problem
-                double[] b = new double[NeuronCell.vertCount];
+            // for solving Ax = b problem
+            double[] b = new double[NeuronCell.vertCount];
 
                
-                var chl = SparseCholesky.Create(l_csc, p);
+            var chl = SparseCholesky.Create(l_csc, p);
                                
-                try
-                {
-                    for (i = 0; i < nT; i++)
+            try
+            {
+                for (i = 0; i < nT; i++)
+                {                       
+                    mutex.WaitOne();
+
+                    r_csc.Multiply(U.ToArray(), b);         // Peform b = rhs * U_curr 
+                    // Diffusion solver
+                //      timer.StartTimer();
+                    chl.Solve(b, b);
+                //    timer.StopTimer(i.ToString());
+
+                    // Set U_next = b
+                    U.SetSubVector(0, NeuronCell.vertCount, Vector.Build.DenseOfArray(b));
+
+                    // Save voltage from diffusion step for state probabilities
+                    tempV.SetSubVector(0, NeuronCell.vertCount, U);
+
+                    // Reaction
+                    channels[0] = na_ONOFF;
+                    channels[1] = k_ONOFF;
+                    channels[2] = leak_ONOFF;
+                    R.SetSubVector(0, NeuronCell.vertCount, reactF(reactConst, U, N, M, H, channels, NeuronCell.boundaryID));
+                    R.Multiply(k / cap, R);
+
+                    // This is the solution for the voltage after the reaction is included!
+                    U.Add(R, U);
+
+                    //Now update state variables using FE on M,N,H
+                    N.Add(fN(tempV, N).Multiply(k), N);
+                    M.Add(fM(tempV, M).Multiply(k), M);
+                    H.Add(fH(tempV, H).Multiply(k), H);
+                                           
+                    // Apply clamp voltages
+                    if (clamps != null && clamps.Count > 0)
                     {
-                        if (SomaOn) { U.SetSubVector(0, NeuronCell.vertCount, setSoma(U, NeuronCell.somaID, vstart)); }
-                        mutex.WaitOne();
-
-                        r_csc.Multiply(U.ToArray(), b);         // Peform b = rhs * U_curr 
-                        // Diffusion solver
-                  //      timer.StartTimer();
-                        chl.Solve(b, b);
-                    //    timer.StopTimer(i.ToString());
-
-                        // Set U_next = b
-                        U.SetSubVector(0, NeuronCell.vertCount, Vector.Build.DenseOfArray(b));
-
-                        // Save voltage from diffusion step for state probabilities
-                        tempV.SetSubVector(0, NeuronCell.vertCount, U);
-
-                        // Reaction
-                        channels[0] = na_ONOFF;
-                        channels[1] = k_ONOFF;
-                        channels[2] = leak_ONOFF;
-                        R.SetSubVector(0, NeuronCell.vertCount, reactF(reactConst, U, N, M, H, channels, NeuronCell.boundaryID));
-                        R.Multiply(k / cap, R);
-
-                        // This is the solution for the voltage after the reaction is included!
-                        U.Add(R, U);
-
-                        //Now update state variables using FE on M,N,H
-                        N.Add(fN(tempV, N).Multiply(k), N);
-                        M.Add(fM(tempV, M).Multiply(k), M);
-                        H.Add(fH(tempV, H).Multiply(k), H);
-
-                        //Always reset to IC conditions and boundary conditions (for now)
-                        //U.SetSubVector(0, NeuronCell.vertCount, boundaryConditions(U, NeuronCell.boundaryID));
-                        if (SomaOn) { U.SetSubVector(0, NeuronCell.vertCount, setSoma(U, NeuronCell.somaID, vstart)); }
-
-                        // Apply clamp voltages
-                        if (clamps != null && clamps.Count > 0)
+                        foreach (NeuronClamp clamp in clamps)
                         {
-                            foreach (NeuronClamp clamp in clamps)
+                            if (clamp != null && clamp.focusVert != -1 && clamp.clampLive)
                             {
-                                if (clamp != null && clamp.focusVert != -1 && clamp.clampLive)
-                                {
-                                    U[clamp.focusVert] = clamp.clampPower;
-                                }
+                                U[clamp.focusVert] = clamp.clampPower;
                             }
                         }
-
-                        mutex.ReleaseMutex();
                     }
-                  
-                }
-                catch (Exception e)
-                {
-                    GameManager.instance.DebugLogErrorThreadSafe(e);
+
                     mutex.ReleaseMutex();
                 }
+                  
+            }
+            catch (Exception e)
+            {
+                GameManager.instance.DebugLogErrorThreadSafe(e);
+                mutex.ReleaseMutex();
+            }
                 
-                GameManager.instance.DebugLogSafe("Simulation Over.");
+            GameManager.instance.DebugLogSafe("Simulation Over.");
             
         }
 
@@ -257,10 +251,6 @@ namespace C2M2.NeuronalDynamics.Simulation
             N[0] = ni;
             H[0] = hi;
 
-            //Set the initial conditions of the solution
-            //U.SetSubVector(0, NeuronCell.vertCount, initialConditions(U, NeuronCell.boundaryID));
-            if (SomaOn) { U.SetSubVector(0, NeuronCell.vertCount, setSoma(U, NeuronCell.somaID, vstart)); }
-           // U.SetSubVector(0, NeuronCell.vertCount, setSoma(U, NeuronCell.somaID, 0));
         }
                 
         // This is for constructing the lhs and rhs of system matrix
@@ -323,37 +313,6 @@ namespace C2M2.NeuronalDynamics.Simulation
             return stencils;
         }
 
-        // this sets the soma voltage for testing
-        // the soma maybe a set of indices, not just one.
-        public static Vector setSoma(Vector V, List<int> somaID, double voltage)
-        {
-           
-            V[0] = voltage;
-            return V;
-        }
-
-        //Function for initializes voltage on cell
-        public static Vector initialConditions(Vector V, List<int> bcIndices)
-        {
-            // only set boundary conditions
-            V.SetSubVector(0, V.Count, boundaryConditions(V, bcIndices));
-
-            return V;
-        }
-
-        // initialize the ends of dendrites to be clamped at 0 mV
-        public static Vector boundaryConditions(Vector V, List<int> bcIndices)
-        {   
-            /*
-            for (int ind = 0; ind < bcIndices.Count; ind++)
-            {
-                V[bcIndices[ind]] = 0;
-            }
-            */
-            return V;
-
-        }
-
         // this is the reaction term of the HH equation
         private static Vector reactF(List<double> reactConst, Vector V, Vector NN, Vector MM, Vector HH, List<bool> channels, List<int> bcIndices)
         {
@@ -374,7 +333,6 @@ namespace C2M2.NeuronalDynamics.Simulation
             if (channels[2]) { output = output + gl * (V - el); }
             // Return the negative of the total
             output.Multiply(-1, output);
-            output.SetSubVector(0, V.Count, boundaryConditions(output, bcIndices));
             return output;
         }
         // The following functions are for the state variable ODEs on M,N,H
