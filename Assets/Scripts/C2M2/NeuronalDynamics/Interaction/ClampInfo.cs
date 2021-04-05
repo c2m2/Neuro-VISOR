62e6d64d2c41cb3635bfeb23de729971fc599e43
CraigFox0 <cefox2001@gmail.com>
2021-04-05 16:36:04 -0400

    Worked on clamp popup

---
+++ Assets/Scripts/C2M2/NeuronalDynamics/Interaction/ClampInfo.cs
@@ -0,0 +1,22 @@
+﻿using C2M2.NeuronalDynamics.Interaction;
+using TMPro;
+using UnityEngine;
+
+public class ClampInfo : MonoBehaviour
+{
+    public TextMeshProUGUI vertexText;
+    public TextMeshProUGUI clampText;
+    private NeuronClamp clamp;
+    //TODO set clamp
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        vertexText.text = clamp.FocusVert.ToString();
+        clampText.text = clamp.ClampPower.ToString();
+    }
+}
