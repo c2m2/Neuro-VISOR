38cd39378190f044121b0a3abb6ab3f5be2a7c07
Calin Pescaru <tun17456@temple.edu>
2022-01-25 00:01:50 -0500

    Added ability to scroll in prev. wnds; track changes in Geometries folder

--- Assets/Scripts/C2M2/NeuronalDynamics/Interaction/CellPreviewer.cs
+++ Assets/Scripts/C2M2/NeuronalDynamics/Interaction/CellPreviewer.cs
@@ -9,6 +9,7 @@ using Random = System.Random;
 namespace C2M2.NeuronalDynamics.Interaction {
     using Utils.DebugUtils;
     using NeuronalDynamics.Visualization;
+    using Utils;
     public class CellPreviewer : MonoBehaviour
     {
         [Tooltip("Cell path relative to StreamingAssets")]
@@ -22,8 +23,19 @@ namespace C2M2.NeuronalDynamics.Interaction {
         public Color32 windowColor = Color.black;
         public GameObject ErrorWindow = null;
 
+        public int maxPrevWnds = 6; // total number of preview windows
+        string[] geoms; // neuron filenames (geometries)
+        string fullPath; // absolute path of the geometries files
+        Vector3[] windowPositions; // preview windows positions
+        Color32[] previewColors; // preview neurons colors
+        GameObject[] wnds; // array of preview windows objects
+        int geomsStartInd = 0; // start position in geoms
+        int geomsEndInd = 0; // end position in geoms
+        bool cantScroll = false; // true if number of neurons is less or equal to number of preview windows
+        static bool folderUpdated = false; // true if Geometries folder was updated
+
         /// <summary>
-        /// Colors ot use for the 1D cell renderings. More than cellColors.Length cells will repeat these colors
+        /// Colors to use for the 1D cell renderings. More than cellColors.Length cells will repeat these colors
         /// </summary>
         public Color32[] cellColors = new Color32[]
         {
@@ -50,6 +62,10 @@ namespace C2M2.NeuronalDynamics.Interaction {
         [Tooltip("If true, positionsNorm will stack above and below on the y axis")]
         public bool stackPos = true;
 
+        // Functions that track the state of Geometries folder
+        private static void OnDeleted(object sender, FileSystemEventArgs e) => folderUpdated = true;
+        private static void OnCreated(object sender, FileSystemEventArgs e) => folderUpdated = true;
+
         private void Awake()
         {
             // Make sure we have window preview prefab and a pointer to a simulation loader
@@ -57,20 +73,34 @@ namespace C2M2.NeuronalDynamics.Interaction {
             FindSimulationLoader();
 
             // Get possible geometries from given direcrory
-            string fullPath = Application.streamingAssetsPath + Path.DirectorySeparatorChar + cellsPath;
-            string[] geoms = GetGeometryNames(fullPath);
+            fullPath = Application.streamingAssetsPath + Path.DirectorySeparatorChar + cellsPath;
+            geoms = GetGeometryNames(fullPath);
+
+            // Setup for tracking Geometries folder
+            var watcher = new FileSystemWatcher(fullPath);
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Created += OnCreated;
+            watcher.Deleted += OnDeleted;
+            watcher.Filter = "*.vrn";
+            watcher.EnableRaisingEvents = true;
+
+            wnds = new GameObject[maxPrevWnds];
+            if (geoms.Length <= maxPrevWnds) cantScroll = true;
 
             if (geoms.Length > 0)
             {
-                if(ErrorWindow != null) ErrorWindow.SetActive(false);
+                if (ErrorWindow != null) ErrorWindow.SetActive(false);
 
-                // Make a preview window for each found geometry
-                Vector3[] windowPositions = GetWindowPositions(geoms.Length);
-                Color32[] previewColors = GetWindowColors(geoms.Length);
-                for (int i = 0; i < windowPositions.Length; i++)
-                {
-                    InstantiatePreviewWindow(geoms[i], windowPositions[i], previewColors[i]);
-                }
+                // Make preview windows; maxPrevWnds in total
+                windowPositions = GetWindowPositions(maxPrevWnds);
+                previewColors = GetWindowColors(geoms.Length);
+                for (int i = 0; i < maxPrevWnds; i++)
+                    wnds[i] = InstantiatePreviewWindow(windowPositions[i]);
+
+                // Populate the preview windows
+                for (int i = 0; i < geoms.Length && i < maxPrevWnds; i++)
+                    PopulatePreviewWindow(wnds[i], geoms[geomsEndInd++], previewColors[i]);
+                --geomsEndInd; // set to actual end index
             }
             else
             {
@@ -115,24 +145,10 @@ namespace C2M2.NeuronalDynamics.Interaction {
                     }
                 }
             }
-            string[] GetGeometryNames(string targetDir)
-            {
-                DirectoryInfo d = new DirectoryInfo(targetDir);
-
-                FileInfo[] files = d.GetFiles("*.vrn");
-                if (files.Length == 0) return new string[] { };
-
-                string[] fileNames = new string[files.Length];
-                for (int i = 0; i < files.Length; i++)
-                {
-                    fileNames[i] = files[i].Name;
-                }
-                return fileNames;
-            }
             Vector3[] GetWindowPositions(int numWindows)
             {
                 // Default Example: 0 <= numWindows <= 18
-                numWindows = Utils.Math.Clamp(numWindows, 0, positionsNorm.Length * 3);
+                // numWindows = Utils.Math.Clamp(numWindows, 0, positionsNorm.Length * 3); // Me
                 if (numWindows == 0) return null;
 
                 // We'll scale the default box positions by the preview window's edge length
@@ -152,41 +168,27 @@ namespace C2M2.NeuronalDynamics.Interaction {
                     }
 
                     // possiblePositions only contains indices 0-5
-                    
+
                     int ind = (i % positionsNorm.Length);
 
 
                     // Copies positions from possiblePositions, stacks if necessary, and scales positions using window length
-                    positions[i] = new Vector3(windowLength.x * positionsNorm[ind].x, 
-                        windowLength.y * (positionsNorm[ind].y + stackAmount), 
+                    positions[i] = new Vector3(windowLength.x * positionsNorm[ind].x,
+                        windowLength.y * (positionsNorm[ind].y + stackAmount),
                         windowLength.z * positionsNorm[ind].z);
-                        
+
                 }
 
                 return positions;
             }
-            Color32[] GetWindowColors(int numColors)
-            {
-                numColors = Utils.Math.Clamp(numColors, 0, positionsNorm.Length * 3);
-                if (numColors == 0) return null;
 
-                Color32[] colors = new Color32[numColors];
-                for (int i = 0; i < colors.Length; i++)
-                {
-                    colors[i] = cellColors[i % cellColors.Length];
-                }
-                Random rnd = new Random();
-
-                // Randomize the order of the colors and return them
-                return colors.OrderBy(x => rnd.Next()).ToArray();
-            }
-            void InstantiatePreviewWindow(string fileName, Vector3 position, Color color)
+            GameObject InstantiatePreviewWindow(Vector3 position)
             {
                 // Instantiate window
                 GameObject go = Instantiate(previewWindowPrefab);
                 go.transform.parent = transform;
                 go.transform.localPosition = position;
-                go.name = fileName + "Preview";
+                go.name = "EmptyPreview";
 
                 // Find each wall in window, color accoring to input
                 MeshRenderer[] prefabWalls = go.GetComponentsInChildren<MeshRenderer>();
@@ -199,13 +201,119 @@ namespace C2M2.NeuronalDynamics.Interaction {
                     }
                 }
 
-                // Start neuron cell previewer.
-                NeuronCellPreview preview = go.GetComponentInChildren<NeuronCellPreview>();
-                preview.vrnFileName = fileName;
-                preview.windowColor = color;
-                preview.loader = loader;
-                preview.PreviewCell(fileName, color);
+                return go;
 
+            }
+        }
+
+        string[] GetGeometryNames(string targetDir)
+        {
+            DirectoryInfo d = new DirectoryInfo(targetDir);
+
+            FileInfo[] files = d.GetFiles("*.vrn");
+            if (files.Length == 0) return new string[] { };
+
+            string[] fileNames = new string[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                fileNames[i] = files[i].Name;
+            }
+            return fileNames;
+        }
+
+        Color32[] GetWindowColors(int numColors)
+        {
+            numColors = Utils.Math.Clamp(numColors, 0, positionsNorm.Length * 3);
+            if (numColors == 0) return null;
+
+            Color32[] colors = new Color32[numColors];
+            for (int i = 0; i < colors.Length; i++)
+            {
+                colors[i] = cellColors[i % cellColors.Length];
+            }
+            Random rnd = new Random();
+
+            // Randomize the order of the colors and return them
+            return colors.OrderBy(x => rnd.Next()).ToArray();
+        }
+
+        void PopulatePreviewWindow(GameObject go, string fileName, Color color)
+        {
+            // Start neuron cell previewer.
+            NeuronCellPreview preview = go.GetComponentInChildren<NeuronCellPreview>();
+            preview.vrnFileName = fileName;
+            preview.windowColor = color;
+            preview.loader = loader;
+            preview.PreviewCell(fileName, color);
+
+            go.name = fileName + "Preview"; // update parent name
+        }
+
+        /// <summary>
+        /// Clear the preview windows by destroying the LineRenderer object
+        /// </summary>
+        void ClearPreviewWnds(GameObject[] o)
+        {
+            for (int i = 0; i < o.Length; i++)
+            {
+                LinesRenderer linesRnd = o[i].GetComponentInChildren<LinesRenderer>();
+                Destroy(linesRnd);
+                o[i].name = "EmptyPreview";
+            }
+        }
+
+        /// <summary>
+        /// Scroll through geoms and update the preview windows.
+        /// If scrollRight is true it will scroll right; otherwise it will scroll left.
+        /// If cantScroll is true the function just returns.
+        /// </summary>
+        public void ScrollPreviewWnds(bool scrollRight)
+        {
+            if (cantScroll) return;
+            else if (!scrollRight && geomsStartInd == 0) return;
+            else if (scrollRight && geomsEndInd == geoms.Length - 1) return;
+
+            for (int i = 0; i < maxPrevWnds; i++)
+            {
+                if (scrollRight)
+                {
+                    if (geomsEndInd == geoms.Length - 1)
+                        break;
+                    geomsStartInd++;
+                    geomsEndInd++;
+                }
+                else
+                {
+                    if (geomsStartInd == 0)
+                        break;
+                    geomsStartInd--;
+                    geomsEndInd--;
+                }
+            }
+
+            ClearPreviewWnds(wnds);
+
+            for (int i = 0, j = geomsStartInd; j <= geomsEndInd; i++, j++)
+                PopulatePreviewWindow(wnds[i], geoms[j], previewColors[i]);
+        }
+
+        void Update()
+        {
+            // Files were added or removed from Geometries folder
+            if (folderUpdated)
+            {
+                geoms = GetGeometryNames(fullPath); // reset geoms
+                previewColors = GetWindowColors(geoms.Length); // reset previewColors
+                folderUpdated = false;
+                cantScroll = false;
+                ClearPreviewWnds(wnds);
+
+                if (geoms.Length <= maxPrevWnds) cantScroll = true;
+                geomsStartInd = geomsEndInd = 0; // reset indices
+
+                for (int i = 0; i < geoms.Length && i < maxPrevWnds; i++)
+                    PopulatePreviewWindow(wnds[i], geoms[geomsEndInd++], previewColors[i]);
+                --geomsEndInd; // set to actual end index
             }
         }
     }
