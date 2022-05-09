a9fb906642875a685ef509df587c86f5b1ed1a3e
Wayne Zheng <wnz6klj@gmail.com>
2022-05-08 21:35:17 -0400

    Adding all available controller prefabs and render automatically the desired controller base on the input device name

---
+++ Assets/Scripts/C2M2/XRScripts/HandPresence.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public bool showController = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach(var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if(prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            {
+                Debug.LogError("Did not find the corresponding controller.");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(showController)
+        {
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+        else
+        {
+            spawnedHandModel.SetActive(true);
+            spawnedController.SetActive(false);
+        }
+    }
+}
