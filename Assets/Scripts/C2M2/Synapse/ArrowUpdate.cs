3a8ac66ee05d51758b855d276eae640051f5d80e
Craig Fox <cefox2001@gmail.com>
2022-05-10 12:40:41 -0400

    synapse colors changed, synapse arrow is colored

--- Assets/Scripts/C2M2/Synapse/ArrowUpdate.cs
+++ Assets/Scripts/C2M2/Synapse/ArrowUpdate.cs
@@ -12,11 +12,18 @@ public class ArrowUpdate : MonoBehaviour
     {
         if (preSynapse != null && postSynapse != null)
         {
-            this.gameObject.transform.SetParent(null);
-            this.gameObject.transform.position = Vector3.Lerp(preSynapse.position, postSynapse.position, 0.5f);
-            this.gameObject.transform.LookAt(postSynapse.position);
-            this.gameObject.transform.localScale = new Vector3(preSynapse.lossyScale.x / 4, preSynapse.lossyScale.x / 4, Vector3.Distance(preSynapse.position, postSynapse.position));
-            this.gameObject.transform.SetParent(preSynapse);
+            transform.SetParent(null);
+            transform.position = Vector3.Lerp(preSynapse.position, postSynapse.position, 0.5f);
+            transform.LookAt(postSynapse.position);
+            transform.localScale = new Vector3(preSynapse.lossyScale.x / 4, preSynapse.lossyScale.x / 4, Vector3.Distance(preSynapse.position, postSynapse.position));
+            transform.SetParent(preSynapse);
+
+            //TODO rewrite this section
+            Color synapseColor = preSynapse.gameObject.GetComponent<Synapse>().meshRenderer.material.color;
+            foreach (MeshRenderer mr in GetComponentsInChildren<MeshRenderer>())
+            {
+                mr.material.color = synapseColor;
+            }
         }
 
     }
