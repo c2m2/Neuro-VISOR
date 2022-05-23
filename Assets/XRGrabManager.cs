a6c182e562cecce0fd0e703ca8cf9aa6df96ffdc
Wayne Zheng <wnz6klj@gmail.com>
2022-05-23 02:49:52 -0400

    Using WaitForSeconds to allow the XRGrabInteractable component to obtain the colliders at runtime

---
+++ Assets/XRGrabManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRGrabManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.childCount>1)
+        {
+            StartCoroutine(CoroutineXRGrab());
+        }
+    }
+
+    IEnumerator CoroutineXRGrab()
+    {
+        yield return new WaitForSeconds(3);
+        gameObject.AddComponent<XRGrabInteractable>();
+    }
+}
