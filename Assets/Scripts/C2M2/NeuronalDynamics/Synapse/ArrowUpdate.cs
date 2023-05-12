37d894b6441f688d431290c0aef89659e88d3fae
Brandon Calia <41372799+brandoncalia@users.noreply.github.com>
2023-05-12 11:37:25 -0400

    Update ArrowUpdate.cs

--- Assets/Scripts/C2M2/NeuronalDynamics/Synapse/ArrowUpdate.cs
+++ Assets/Scripts/C2M2/NeuronalDynamics/Synapse/ArrowUpdate.cs
@@ -4,6 +4,10 @@ public class ArrowUpdate : MonoBehaviour
 {
     public Transform preSynapse;
     public Transform postSynapse;
+    
+    public Synapse pre;
+    public Synapse post;
+    
 
     /// <summary>
     /// A script to update arrow direction and size when the user has moved the neuron
@@ -17,15 +21,28 @@ public class ArrowUpdate : MonoBehaviour
             transform.LookAt(postSynapse.position);
             transform.localScale = new Vector3(preSynapse.lossyScale.x / 4, preSynapse.lossyScale.x / 4, Vector3.Distance(preSynapse.position, postSynapse.position));
             transform.SetParent(preSynapse);
-
-            //TODO rewrite this section
-            Color preSynapseColor = preSynapse.GetComponent<Synapse>().meshRenderer.material.color;
-            Color newColor = new Color(preSynapseColor.r, preSynapseColor.g, preSynapseColor.b, 0.5f);
+            
+            Color newColor = new Color(0,0,0);
+            // Assign color based on the synapse model 
+            if (pre.currentModel == Synapse.Model.NMDA)
+            {
+                // NMDA color scheme
+                newColor.r = 0f;
+                newColor.g = .576f;
+                newColor.b = .391f;
+            }
+            else 
+            {
+                // GABA color scheme
+                newColor.r = .627f;
+                newColor.g = 0f;
+                newColor.b = .145f;
+            }
+            
             foreach (MeshRenderer mr in GetComponentsInChildren<MeshRenderer>())
             {
                 mr.material.color = newColor;
             }
         }
-
     }
 }
