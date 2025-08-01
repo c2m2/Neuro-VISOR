c63073b680f03704cd6b1317717f21d5e63711bb
zmiksis <zmiksis@gmail.com>
2025-08-01 16:52:38 -0400

    Definitions updated
    Missing definitions updated and corrected.

--- Assets/Scripts/C2M2/NeuronalDynamics/Simulation/NDSimulation.cs
+++ Assets/Scripts/C2M2/NeuronalDynamics/Simulation/NDSimulation.cs
@@ -31,6 +31,8 @@ namespace C2M2.NeuronalDynamics.Simulation {
         public CharacteristicDistance characteristicDistance {get; set; } = null;
         public new NDSimulationManager Manager { get { return GameManager.instance.simulationManager; } }
         private double visualInflation = 1;
+        public CSVWriter csv = null;
+        public SparseSolverTestv1 solver = null;
         public double VisualInflation
         {
             get { return visualInflation; }
@@ -357,6 +359,26 @@ namespace C2M2.NeuronalDynamics.Simulation {
         /// </summary>
         /// <returns></returns>
         public abstract double[] Get1DValues ();
+         protected override async void WriteCSV()
+        {   
+            
+            if (csv != null)
+            {
+                csv.WriteToCSV(1000 * GetSimulationTime(), Get1DValues());
+            }
+        }
+
+        public bool convert = false;
+
+        protected override async void StopCSV()
+        {
+            if (convert == true)
+            {   convert = false;
+                csv.ConvertToCSV();
+                csv = null;
+
+            }
+        }
         protected override void OnAwakePre()
         {
             UpdateGrid1D();
