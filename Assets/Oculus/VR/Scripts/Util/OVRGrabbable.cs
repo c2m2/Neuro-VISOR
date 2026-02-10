c00d95351fdfa23c7f179dc5396e527a06be5cce
zmiksis <zmiksis@gmail.com>
2026-02-10 13:03:02 -0500

    Package upgrades
    Upgrades to package versions.

--- Assets/Oculus/VR/Scripts/Util/OVRGrabbable.cs
+++ Assets/Oculus/VR/Scripts/Util/OVRGrabbable.cs
@@ -126,7 +126,7 @@ public class OVRGrabbable : MonoBehaviour
     {
         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
         rb.isKinematic = m_grabbedKinematic;
-        rb.velocity = linearVelocity;
+        rb.linearVelocity = linearVelocity;
         rb.angularVelocity = angularVelocity;
         m_grabbedBy = null;
         m_grabbedCollider = null;
